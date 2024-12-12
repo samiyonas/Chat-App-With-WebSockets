062945c48804a80a9cf1f17b9b4ca1f528b8d900
samiyonas <samuel.x.yonas@gmail.com>
2024-12-12 17:18:03 +0300

    home page served from template

--- Chat-App-With-WebSockets/main.go
+++ Chat-App-With-WebSockets/main.go
@@ -3,23 +3,25 @@ package main
 import (
     "log"
     "net/http"
+    "html/template"
+    "sync"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(`
-    <html>
-    <head>
-    <title>Chat</title>
-    </head>
-    <body>
-    Let's chat!
-    </body>
-    </html>
-    `))
+type templateHandler struct {
+    once sync.Once
+    filename string
+    tmpl *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    t.once.Do(func() {
+        t.tmpl = template.Must(template.ParseFiles("templates/"+t.filename))
+    })
+    t.tmpl.Execute(w, nil)
 }
 
 func main() {
-    http.HandleFunc("/", Home)
+    http.Handle("/", &templateHandler{filename: "chat.html"})
 
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal("ListenAndServe:", err)
