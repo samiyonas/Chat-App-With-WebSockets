ee689aa1991b26e78e64f75ab9ad1cbe6306713f
samiyonas <samuel.x.yonas@gmail.com>
2024-12-13 13:52:03 +0300

    documenting

--- Chat/main.go
+++ Chat/main.go
@@ -7,12 +7,17 @@ import (
     "sync"
 )
 
+// Handles templates  for the home page
 type templateHandler struct {
+    // Makes sure that template compiling is done once for all goroutines
     once sync.Once
+    // template filename
     filename string
+    // Template type
     tmpl *template.Template
 }
 
+// Using the above type as a handler by implementing http.Handler interface(ServeHTTP method)
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     t.once.Do(func() {
         t.tmpl = template.Must(template.ParseFiles("templates/"+t.filename))
@@ -21,8 +26,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    // Registers the handler for the home page which is templateHandler type
     http.Handle("/", &templateHandler{filename: "chat.html"})
 
+    // Error handling
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal("ListenAndServe:", err)
     }
